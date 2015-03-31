3cd53284d6408649fb5719fb36cb0e1cd5adf4fa
ZaL <z4ls@live.com>
2015-03-31 19:12:36 +0300

    insta final ver

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,7 +15,7 @@ class UserTableSeeder extends Seeder {
     {
         User::create([
             'name' => 'Mubarak',
-            'email' => 'mubarakesmail2@gmail.com',
+            'email' => 'mubarakesmail3@gmail.com',
             'password' => Hash::make('123'),
             'active' => 1
         ]);
