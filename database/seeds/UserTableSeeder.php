4a6179c3596ddb12edcadb2a69a4eb2d86da6093
ZaL <z4ls@live.com>
2015-03-31 18:56:38 +0300

    insta final ver

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,30 +14,6 @@ class UserTableSeeder extends Seeder {
     public function run()
     {
         User::create([
-            'name' => 'ZaL',
-            'email' => 'z4ls@live.com',
-            'password' => Hash::make('darkage'),
-            'active' => 1
-        ]);
-        User::create([
-            'name' => 'Hamdhan',
-            'email' => 'h4mdh4n@live.com',
-            'password' => Hash::make('darkage'),
-            'active' => 1
-        ]);
-        User::create([
-            'name' => 'Faiz',
-            'email' => 'faiz@live.com',
-            'password' => Hash::make('darkage'),
-            'active' => 1
-        ]);
-        User::create([
-            'name' => 'Hashir',
-            'email' => 'hashir@live.com',
-            'password' => Hash::make('darkage'),
-            'active' => 1
-        ]);
-        User::create([
             'name' => 'Mubarak',
             'email' => 'mubarakesmail@gmail.com',
             'password' => Hash::make('instapassword'),
