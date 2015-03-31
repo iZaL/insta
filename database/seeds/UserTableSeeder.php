0aef71037056d1d1200ce507b9b27c910d17048a
ZaL <z4ls@live.com>
2015-03-31 19:09:46 +0300

    insta final ver

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,7 +15,7 @@ class UserTableSeeder extends Seeder {
     {
         User::create([
             'name' => 'Mubarak',
-            'email' => 'mubarakesmail@gmail.com',
+            'email' => 'mubarakesmail2@gmail.com',
             'password' => Hash::make('instapassword'),
             'active' => 1
         ]);
