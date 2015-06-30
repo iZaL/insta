b41fa75fb3e0fc13c25655892b9eea2c31b7717d
ZaL <z4ls@live.com>
2015-06-30 22:39:42 +0300

    fix

--- app/Http/Controllers/InstagramsController.php
+++ app/Http/Controllers/InstagramsController.php
@@ -223,9 +223,9 @@ class InstagramsController extends Controller
         }
 
         foreach ($accounts as $username => $account) {
-            if (trim(!(empty($account->pagination)) || !(is_null($account->pagination)) || !(isset($account->pagination)) || !($account->pagination == ''))) {
+//            if (trim(!(empty($account->pagination)) || !(is_null($account->pagination)) || !(isset($account->pagination)) || !($account->pagination == ''))) {
 
-//            if (isset($account->pagination) && !empty(get_object_vars($account->pagination))) {
+            if (!empty(get_object_vars($account->pagination))) {
                 $response[$username]['pagination'] = $account->pagination->next_max_like_id;
             }
             $response[$username]['username'] = $username;
